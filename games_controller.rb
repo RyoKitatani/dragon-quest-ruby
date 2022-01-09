0fe8d5603b8118d0863e9f639ab963d73a3e6f22
RyoKitatani <ryo0929m@gmail.com>
2022-01-09 14:52:46 +0900

    finished_4-5

--- games_controller.rb
+++ games_controller.rb
@@ -4,40 +4,48 @@ class GamesController
   GOLD_CONSTANT = 2
 
   def battle(**params)
-    brave = params[:brave]
-    monster = params[:monster]
+    build_characters(params)
 
     # ループ処理
     loop do
-      brave.attack(monster)
-      break if battle_end(monster)
-      monster.attack(brave)
-      break if battle_end(brave)
-    end
+      @brave.attack(@monster)
+      break if battle_end?
+      @monster.attack(@brave)
+      break if battle_end?
 
-    if battle_result(brave)
-      result = calculate_of_exp_gold(monster)
-      puts "#{brave.name}は戦いに勝利した"
-      puts "#{result[:exp]}の経験値と#{result[:gold]}ゴールドを獲得した!"
-    else
-      puts "#{brave.name}は戦いに負けた"
-      puts "目の前が真っ暗になった......"
+      battle_judgement
     end
   end
 
   private
 
-  def battle_result(brave)
-    brave.hp > 0
+  def build_characters(**params)
+    @brave = params[:brave]
+    @monster = params[:monster]
   end
 
-  def battle_end(character)
-    character.hp <= 0
+  def battle_win?
+    @brave.hp > 0
   end
 
-  def calculate_of_exp_gold(monster)
-    exp = (monster.offense + monster.defense) * EXP_CONSTANT
-    gold = (monster.offense + monster.defense) * GOLD_CONSTANT
+  def battle_end?
+    @brave.hp <= 0 || @monster.hp <= 0
+  end
+
+  def battle_judgement
+    if battle_win?
+      result = calculate_of_exp_gold
+      puts "#{@brave.name}は戦いに勝利した"
+      puts "#{result[:exp]}の経験値と#{result[:gold]}ゴールドを獲得した!"
+    else
+      puts "#{@brave.name}は戦いに負けた"
+      puts "目の前が真っ暗になった......"
+    end
+  end
+
+  def calculate_of_exp_gold
+    exp = (@monster.offense + @monster.defense) * EXP_CONSTANT
+    gold = (@monster.offense + @monster.defense) * GOLD_CONSTANT
     result = {exp: exp, gold: gold}
 
     result
