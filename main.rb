f18f6ddc8b814bced21176d2f5db4dc6fbc1fe78
RyoKitatani <ryo0929m@gmail.com>
2022-01-09 14:18:55 +0900

    finished_3-8

--- main.rb
+++ main.rb
@@ -125,12 +125,21 @@ monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 # ループ処理
 loop do
   brave.attack(monster)
-
   break if monster.hp <= 0
 
   monster.attack(brave)
-
   break if brave.hp <= 0
 end
 
+battle_result = brave.hp > 0
+
+if battle_result
+  exp = (monster.offense + monster.defense) * 2
+  gold = (monster.offense + monster.defense) * 3
+  puts "#{brave.name}は戦いに勝利した"
+  puts "#{exp}の経験値と#{}ゴールドを獲得した!"
+else
+  puts "#{brave.name}は戦いに負けた"
+  puts "目の前が真っ暗になった......"
+end
 
